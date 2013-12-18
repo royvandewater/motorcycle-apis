3b6e5f4f3d46d9ff1c9511b53017064db514718e
Jade Meskill and Roy van de Water <dev+jade+roy@integrumtech.com>
2013-12-18 09:07:32 -0700

    Add Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MotorcycleApis::Application.config.secret_key_base = '5a04a75670d251f15323297e08d64696d06e15be1be32cdf002ced3986ce581473cf4386fc3bff4bda0e662d9b3737a95737efec36d7353e085537d432cf0b62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MotorcycleApis::Application.config.session_store :cookie_store, key: '_motorcycle-apis_session'
